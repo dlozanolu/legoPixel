10eb331216da2364c04bab10ffabc1637f09dfb8
David Lozano <david@afterbanks.com>
2020-06-02 01:35:24 +0200

    Hago que me de las instrucciones por voz (en Mac)

--- leeFotoLego.php
+++ leeFotoLego.php
@@ -1,37 +1,53 @@
 <?php
+$mostrarMapa = false; //para hacerme una idea de los colores
+$colorAMostrar = 0; //si vale -1 muestra todos.
 $nombreDelFichero = 'lanzadera.min.gif';
 $dimensiones = getimagesize($nombreDelFichero);
 $ancho = $dimensiones[0];
 $alto = $dimensiones[1];
 $contador = 0;
-$totalColores = [];
 $imagen = imagecreatefromgif($nombreDelFichero);
+$handle = fopen("php://stdin", "r");
 
 for ($y=0; $y < $alto; $y++) {
-    echo "Línea $y: ";
+    $texto = "Línea " . ($y+1) . ": ";
+    echo $texto;
+    if (!$mostrarMapa) {
+        shell_exec('say "' . $texto . '"');
+        fgets($handle);
+    }
 
     for ($x=0; $x < $ancho; $x++) {
-        if ($x == 0) {
-            $contador = 0;
-        }
-        $colorIndice = imagecolorat($imagen, $x, $y);
-        if (!array_key_exists($color, $totalColores)) {
-            array_push($totalColores, $colorIndice);
-        }
-        $color = array_search($colorIndice, $totalColores);
-        if ($x == ($ancho - 1)) {
-            $colorSiguiente = -1;
+        $color = imagecolorat($imagen, $x, $y);
+
+        if ($mostrarMapa) {
+            echo $color;
         } else {
-            $colorIndiceSiguiente = imagecolorat($imagen, $x + 1, $y);
-            $colorSiguiente = array_search($colorIndiceSiguiente, $totalColores);
-        }
-    
-        if ($color == $colorSiguiente) {
-            $contador++;
-        } else {
-            //echo $color . 'x' . ($contador+1) . ' ';
-            echo $color . 'x' . ($contador-$x) . ' al ' . ($x) . ' ';
-            $contador = 0;
+            if ($x == 0) {
+                $contador = 0;
+            }
+            if ($x == ($ancho - 1)) {
+                $colorSiguiente = -1;
+            } else {
+                $colorSiguiente = imagecolorat($imagen, $x + 1, $y);
+            }
+
+            if ($color == $colorSiguiente) {
+                $contador++;
+            } else {
+                //echo $color . 'x' . ($contador+1) . ' ';
+                if ($colorAMostrar == -1) {
+                    echo $color . 'x' . ($x-$contador+1) . ' al ' . ($x+1) . ' ';
+                } else {
+                    if ($color == $colorAMostrar) {
+                        $texto = ($x-$contador+1) . ' al ' . ($x) . '. ';
+                        echo $texto;
+                        shell_exec('say "' . $texto . '"');
+                        fgets($handle);
+                    }
+                }
+                $contador = 0;
+            }
         }
     }
     echo "\n";
